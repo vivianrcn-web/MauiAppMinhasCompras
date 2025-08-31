ada5d80628ba66d195ee70f079f630182e73acc0
Vivian Costa <vivian_rcostan@outlook.com>
2025-08-31 20:19:45 -0300

    agenda04, apoio de video aula ia

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -5,8 +5,9 @@ namespace MauiAppMinhasCompras.Models
     {
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
+        public string? Descricao { get; set; }
         public double Quantidade { get; set; }
         public double Preco { get; set; }
+        public double Total { get => Quantidade * Preco; }
     }
 }
