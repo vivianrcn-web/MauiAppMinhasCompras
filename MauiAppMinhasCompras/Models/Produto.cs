211d956292d86ebec71cda1cd425c4fbc3353f85
Vivian Costa <vivian_rcostan@outlook.com>
2025-09-14 19:25:38 -0300

    .

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -1,9 +1,13 @@
 ﻿using SQLite;
+using System;
+
 namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
         string _descricao;
+        double _quantidade;
+        double _preco;
 
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
@@ -19,8 +23,30 @@ namespace MauiAppMinhasCompras.Models
                 _descricao = value;
             }
         }
-        public double Quantidade { get; set; }
-        public double Preco { get; set; }
+        public double Quantidade {
+            get => _quantidade;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Por favor, preencha a quantidade maior do que zero");
+                }
+
+                _quantidade = value;
+            }
+        }
+        public double Preco {
+            get => _preco;
+            set 
+            {
+                if (value == 0)
+                {
+                    throw new Exception("Por favor, preencha o preço maior que zero");
+                }
+            
+            }
+                
+        }
         public double Total { get => Quantidade * Preco; }
     }
 }
